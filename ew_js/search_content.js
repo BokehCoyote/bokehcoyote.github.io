068646958c5966a27ca8c4578eb513454474424a
BokehCoyote <122295701+BokehCoyote@users.noreply.github.com>
2023-01-09 18:01:24 -0600

    Add files via upload

--- ew_js/search_content.js
+++ ew_js/search_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{"title":"Mobile","text":" Gay sex","tags":"","url":"index.html","UUID":"99E141D7D9FF4505951C216C0A459643"}]};
+var tipuesearch = {"pages": [{"title":"Mobile","text":" Welcome to my cozy corner of the internet! Pick one of these awful platforms to view my art on","tags":"","url":"index.html","UUID":"99E141D7D9FF4505951C216C0A459643"}]};
