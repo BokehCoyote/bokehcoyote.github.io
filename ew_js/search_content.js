bced66d8cf71f2d3e81eb7a6aa269edacf6b1e21
BokehCoyote <122295701+BokehCoyote@users.noreply.github.com>
2023-01-09 18:17:11 -0600

    Add files via upload

--- ew_js/search_content.js
+++ ew_js/search_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{"title":"Mobile","text":" Welcome to my cozy corner of the internet! Pick one of these awful platforms to view my art on","tags":"","url":"index.html","UUID":"99E141D7D9FF4505951C216C0A459643"}]};
+var tipuesearch = {"pages": [{"title":"Mobile","text":" Site currently under construction. Not like anyone gonna see this but if you wanna find me on socials click below Twitter Telegram Channel Instagram Furaffinity","tags":"","url":"index.html","UUID":"99E141D7D9FF4505951C216C0A459643"}]};
